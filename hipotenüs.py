0eef6a3af7a7bbea0b16412ccd6b153847cbb604
busraugurx <busraugur934@gmail.com>
2021-05-28 01:48:58 +0300

    Initial commit

---
+++ hipotenüs.py
@@ -0,0 +1,6 @@
+print("***DİK ÜÇGENİN HİPOTENÜSÜNÜ HESAPLAMA***")
+"a^2+b^2=c^2"
+a=int(input("ÜÇGENİN A KENARI İÇİN BİR DEĞER GİRİN: "))
+b=int(input("ÜÇGENİN B KENARI İÇİN BİR DEĞER GİRİN: "))
+c = (a*a + b*b)**0.5
+print("HİPOTENÜS: ",c)
